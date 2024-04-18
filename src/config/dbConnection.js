3bf95ba14d1cebb084cea10f0f332758c2c5fb2c
subhranil002 <106914208+subhranil002@users.noreply.github.com>
2024-04-18 23:36:22 +0530

    Added configuration files

---
+++ src/config/dbConnection.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+import { config } from "dotenv";
+config();
+
+mongoose.set("strictQuery", false);
+
+const connectionToDb = async () => {
+    await mongoose
+        .connect(process.env.MONGO_URI, {
+            dbName: process.env.DB_NAME,
+        })
+        .then((data) => {
+            console.log(`Connected to DB: ${data.connection.host}`);
+        })
+        .catch((error) => {
+            console.log(error.message);
+        });
+};
+
+export default connectionToDb;
