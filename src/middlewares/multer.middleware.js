aa698cd4be6e1b6fef540558a46610559b4fd877
subhranil002 <106914208+subhranil002@users.noreply.github.com>
2024-04-21 00:49:27 +0530

    Added userSchema, courseSchema

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -2,13 +2,13 @@ import path from "path";
 import multer from "multer";
 
 const upload = multer({
-    dest: "uploads",
+    dest: "./public/temp",
     limits: { fileSize: 1024 * 1024 * 1024 * 1024 },
     storage: multer.diskStorage({
-        destination: "uploads",
+        destination: "./public/temp",
         filename: (_req, file, cb) => {
             cb(null, file.originalname);
-        },
+        }
     }),
     fileFilter: (_req, file, cb) => {
         let ext = path.extname(file.originalname);
@@ -25,7 +25,7 @@ const upload = multer({
         }
 
         cb(null, true);
-    },
+    }
 });
 
 export default upload;
---
+++ src/models/user.model.js
@@ -0,0 +1,113 @@
+import mongoose, { Schema, model } from "mongoose";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { config } from "dotenv";
+config();
+import crypto from "crypto";
+
+const userSchema = new Schema(
+    {
+        fullName: {
+            type: "String",
+            required: [true, "Name is required"],
+            lowercase: true,
+            trim: true
+        },
+        email: {
+            type: "String",
+            required: [true, "Email is required"],
+            lowercase: true,
+            trim: true,
+            unique: true,
+            match: [
+                /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                "Please fill in a valid email address"
+            ]
+        },
+        password: {
+            type: "String",
+            required: [true, "Password is required"],
+            minLength: [8, "Password must be at least 8 characters"],
+            select: false
+        },
+        avtar: {
+            public_id: {
+                type: "String"
+            },
+            secure_url: {
+                type: "String"
+            }
+        },
+        role: {
+            type: "String",
+            enum: ["USER", "ADMIN"],
+            default: "USER"
+        },
+        forgotPasswordToken: String,
+        forgotPasswordExpiry: Date,
+        subscription: {
+            id: {
+                type: String,
+                default: ""
+            },
+            status: {
+                type: String,
+                enum: [
+                    "Active",
+                    "Pending",
+                    "Halted",
+                    "Cancelled",
+                    "Paused",
+                    "Expired",
+                    "Completed"
+                ],
+                default: "Completed"
+            }
+        }
+    },
+    {
+        timestamps: true
+    }
+);
+
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) {
+        return next();
+    }
+
+    this.password = await bcrypt.hash(this.password, 10);
+});
+
+userSchema.methods = {
+    generateJWTToken: async function () {
+        return await jwt.sign(
+            {
+                id: this._id,
+                email: this.email,
+                subscription: this.subscription,
+                role: this.role
+            },
+            process.env.JWT_SECRET,
+            {
+                expiresIn: process.env.JWT_EXPIRY
+            }
+        );
+    },
+    comparePassword: async function (plainTextPassword) {
+        return await bcrypt.compare(plainTextPassword, this.password);
+    },
+    generatePasswordResetToken: async function () {
+        const resetToken = crypto.randomBytes(20).toString("hex");
+
+        this.forgotPasswordToken = await crypto
+            .createHash("sha256")
+            .update(resetToken)
+            .digest("hex");
+        this.forgotPasswordExpiry = Date.now() + 15 * 60 * 1000;
+
+        return resetToken;
+    }
+};
+
+const User = model("user", userSchema);
+export default User;
